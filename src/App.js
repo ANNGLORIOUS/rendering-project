fc4ed7c406513df30acc8021a37da15ca8ce7591
ANNGLORIOUS <mueniannglorious@gmail.com>
2024-08-01 14:57:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
+  handleLogin = () => {
+    this.setState({ isLoggedIn: true });
+  };
+
+  handleLogout = () => {
+    this.setState({ isLoggedIn: false });
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          {this.state.isLoggedIn ? (
+            <p>Welcome back!</p>
+          ) : (
+            <p>Please log in.</p>
+          )}
+        </div>
+        <div>
+          {this.state.isLoggedIn ? (
+            <button onClick={this.handleLogout}>Log Out</button>
+          ) : (
+            <button onClick={this.handleLogin}>Log In</button>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
